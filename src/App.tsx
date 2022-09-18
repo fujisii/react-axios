5af75a3d4748e70f24421ee7938f725933e61125
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-18 21:28:52 +0900

    POST リクエストを送信する方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 import axios from "axios";
 import React from "react";
 
@@ -9,17 +7,29 @@ export default function App() {
   const [post, setPost] = React.useState(null);
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
+    axios.get(`${baseURL}/1`).then((response) => {
       setPost(response.data);
     });
   }, []);
 
-  if (!post) return null;
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return "No post!"
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
