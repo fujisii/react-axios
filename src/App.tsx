eef397491b9e117e5d9bd7c713fb0b7369d85dc3
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-22 07:52:55 +0900

    POST リクエストを送信する方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import axios from "axios";
 import React from "react";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export default function App() {
   const [post, setPost] = React.useState(null);
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
+    axios.get(`${baseURL}/1`).then((response) => {
       setPost(response.data);
     });
   }, []);
 
-  if (!post) return null;
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return "No post!";
   console.log(post);
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
