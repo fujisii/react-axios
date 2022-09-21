2571304058c45ef6bd8d7d962bc531273efd5d01
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-21 12:46:21 +0900

    GET リクエストを送信する方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
 import axios from "axios";
 import React from "react";
 
-const baseURL = "urlを指定する";
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
 
 export default function App() {
   const [post, setPost] = React.useState(null);
 
   React.useEffect(() => {
-    axios.get(`${baseURL}/1`).then((response) => {
+    axios.get(baseURL).then((response) => {
       setPost(response.data);
     });
   }, []);
 
-  function createPost() {
-    axios
-      .post(baseURL, {
-        title: "Hello World!",
-        body: "This is a new post."
-      })
-      .then((response) => {
-        setPost(response.data);
-      });
-  }
-
-  if (!post) return "No post!"
+  if (!post) return null;
+  console.log(post);
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
-      <button onClick={createPost}>Create Post</button>
     </div>
   );
 }
