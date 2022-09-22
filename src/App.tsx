06d2a65e53b3eac448e743d4ed945109a148dee9
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-22 12:30:54 +0900

    ファイル構成を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import axios from "axios";
-import React from "react";
+import "./styles.css";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts";
+import AxiosPost from "./axios/post";
 
 export default function App() {
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get(`${baseURL}/1`).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  function createPost() {
-    axios
-      .post(baseURL, {
-        title: "Hello World!",
-        body: "This is a new post."
-      })
-      .then((response) => {
-        setPost(response.data);
-      });
-  }
-
-  if (!post) return "No post!";
-  console.log(post);
-
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-      <button onClick={createPost}>Create Post</button>
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <AxiosPost />
     </div>
   );
 }
