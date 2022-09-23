b73bd2672a0bb697d2875250089a34d539c0e451
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-23 17:01:45 +0900

    DELETE リクエストを送信する方法

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import AxiosPost from "./axios/post";
 import AxiosPut from "./axios/put";
+import AxiosDelete from "./axios/delete";
 
 export default function App() {
   return (
@@ -9,7 +10,8 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       {/* <AxiosPost /> */}
-      <AxiosPut />
+      {/* <AxiosPut /> */}
+      <AxiosDelete />
     </div>
   );
 }
