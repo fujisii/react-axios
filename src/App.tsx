2b5504292eb19f58c7bf13101ae13fd24b7c3a98
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-23 16:56:39 +0900

    PUT リクエストを送信する方法

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./styles.css";
 
 import AxiosPost from "./axios/post";
+import AxiosPut from "./axios/put";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <AxiosPost />
+      {/* <AxiosPost /> */}
+      <AxiosPut />
     </div>
   );
 }
